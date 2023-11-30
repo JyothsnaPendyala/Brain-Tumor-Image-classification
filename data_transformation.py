168a928559e8b10a1e03eb6e0e5097cd9528f5e3
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-11-30 23:09:57 +0530

    Update data_transformation.py

--- data_transformation.py
+++ data_transformation.py
@@ -9,7 +9,8 @@ import torch
 import dill as pickle
 
 def transform_data():
-    glioma_tumor_image_urls,meningioma_tumor_image_urls,no_tumor_image_urls,pituitory_tumor_image_urls = visualise_image()
+    path,glioma_tumor_images,meningioma_tumor_images,no_tumor_images,pituitory_tumor_images = visualise_image()
+    
     data_transform = torchvision.transforms.Compose(
     [
         transforms.Resize(256),
@@ -17,45 +18,13 @@ def transform_data():
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    # tumor_list = [glioma_tumor_image_urls,meningioma_tumor_image_urls,no_tumor_image_urls,pituitory_tumor_image_urls]
-    # images = []
-    # for lst in tumor_list:
-    #     for url in lst:
-    #         response = requests.get(url)
-    #         image = Image.open(BytesIO(response.content))
-    #         images.append(image)
-    
-    # transformed_images = [data_transform(img) for img in images]
-    # model_dataset = torch.stack(transformed_images)
-    
-
-    # model_dataset = datasets.ImageFolder(path, transform=data_transform)
-
-    def download_image(url):
-        response = requests.get(url)
-        img = Image.open(BytesIO(response.content))
-        return img
-    
-    class CustomImageDataset(Dataset):
-        def __init__(self, image_urls, transform=None):
-            self.image_urls = image_urls
-            self.transform = transform
-
-        def __len__(self):
-            return len(self.image_urls)
-
-        def __getitem__(self, idx):
-            img = download_image(self.image_urls[idx])
-            if self.transform:
-                img = self.transform(img)
-            return img
-    
-    all_image_urls = glioma_tumor_image_urls+meningioma_tumor_image_urls+no_tumor_image_urls+pituitory_tumor_image_urls
-    model_dataset = CustomImageDataset(all_image_urls, transform=data_transform)
-    img = model_dataset[0]
-    print("img:",img)
-    # print("ann:",ann)
+    total_list = glioma_tumor_images + meningioma_tumor_images + no_tumor_images + pituitory_tumor_images
+    model_dataset = datasets.ImageFolder(path, transform=data_transform)
+    img, ann = model_dataset[0]
+    print("iiiiiiii",img)
+    print("aaaaaaaaa:",ann)
     with open('model_dataset.pkl', 'wb') as f:
         pickle.dump(model_dataset, f)
     return model_dataset
+
 transform_data()
